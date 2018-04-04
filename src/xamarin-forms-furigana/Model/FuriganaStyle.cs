19e2bb13492ef666ffaa99163697b4a2215a336d
ANDY840119-PC\andy840119 <andy840119@gmail.com>
2018-04-04 15:26:40 +0900

    fix change property but not notify change

--- src/xamarin-forms-furigana/Model/FuriganaStyle.cs
+++ src/xamarin-forms-furigana/Model/FuriganaStyle.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using furigana.Annotations;
 
 namespace furigana.Model
 {
@@ -7,36 +9,104 @@ namespace furigana.Model
     /// </summary>
     public class FuriganaStyle : INotifyPropertyChanged
     {
-        /// <summary>
-        ///     size
-        /// </summary>
-        public double FuriganaFontSize { get; set; } = 8;
+        private double _furiganaFontSize = 8;
+        private double _characterFontSize = 15;
+        private double _romajiFontSize = 7;
+        private double _characterSpacing = 1;
+        private double _furiganaSpacing;
+        private double _romajiSpacing;
 
         /// <summary>
         ///     size
         /// </summary>
-        public double CharacterFontSize { get; set; } = 15;
+        public double FuriganaFontSize
+        {
+            get => _furiganaFontSize;
+            set
+            {
+                _furiganaFontSize = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         ///     size
         /// </summary>
-        public double RomajiFontSize { get; set; } = 7;
+        public double CharacterFontSize
+        {
+            get => _characterFontSize;
+            set
+            {
+                _characterFontSize = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <summary>
+        ///     size
+        /// </summary>
+        public double RomajiFontSize
+        {
+            get => _romajiFontSize;
+            set
+            {
+                _romajiFontSize = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         ///     spacing between chatacters
         /// </summary>
-        public double CharacterSpacing { get; set; } = 1;
+        public double CharacterSpacing 
+        {
+            get => _characterSpacing;
+            set
+            {
+                _characterSpacing = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         ///     spacing between furigana and character
         /// </summary>
-        public double FuriganaSpacing { get; set; } = 0;
+        public double FuriganaSpacing
+        {
+            get => _furiganaSpacing;
+            set
+            {
+                _furiganaSpacing = value;
+                OnPropertyChanged();
+            }
+        }
 
         /// <summary>
         ///     spacing between romaji and character
         /// </summary>
-        public double RomajiSpacing { get; set; } = 0;
+        public double RomajiSpacing
+        {
+            get => _romajiSpacing;
+            set
+            {
+                _romajiSpacing = value;
+                OnPropertyChanged();
+            }
+        }
 
+        /// <summary>
+        /// Event
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
+
+        /// <summary>
+        /// invoke
+        /// </summary>
+        /// <param name="propertyName"></param>
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
