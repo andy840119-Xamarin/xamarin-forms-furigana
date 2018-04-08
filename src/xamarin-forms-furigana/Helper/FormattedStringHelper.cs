7fd7e088a92fda174e6f6071c55b26b4bd73fb58
為什麼 <andy840119@gmail.com>
2018-04-08 14:52:44 +0900

    add helper

---
+++ src/xamarin-forms-furigana/Helper/FormattedStringHelper.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Furigana.Helper
+{
+    /// <summary>
+    /// Convert text to FormattedString
+    /// </summary>
+    public static class FormattedStringHelper
+    {
+        /// <summary>
+        /// Gemerate Vertical text
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static FormattedString MakeTextVertical(string str)
+        {
+            FormattedString formattedString = new FormattedString();
+            
+            //generate lise spans
+            for(int i=0;i<str.Length;i++)
+            {
+                //text
+                formattedString.Spans.Add(new Span() { Text = str[i].ToString() });
+                //new line
+                if(i< str.Length-1)
+                    formattedString.Spans.Add(new Span { Text = Environment.NewLine });
+
+            }
+
+            return formattedString;
+        }
+
+        /// <summary>
+        /// Gemerate Vertical text
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static FormattedString MakeTextHorizontal(string str)
+        {
+            return new FormattedString
+            {
+                Spans =
+                {
+                    new Span { Text = str },        
+                }
+            };
+        }
+    }
+}
